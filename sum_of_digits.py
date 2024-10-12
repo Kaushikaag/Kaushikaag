ae15b040cc17fb19c0d6380a0dae5f4b3c3771be
Kaushika Aggrawal <kaushikaag06@gmail.com>
2024-10-12 14:47:55 +0530

    sum.py

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -1,9 +1,9 @@
 number = int(input("Enter a number: "))
 
-sum_of_digits = 0
+sum = 0
 while number > 0:
     digit = number % 10
-    sum_of_digits += digit
+    sum += digit
     number = number / 10
 
-print("The sum of the digits is:", sum_of_digits)
+print("The sum of the digits is:", sum)
